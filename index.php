9e13753fe6b193caba54a8b17c9526164bb4378f
yuldash <150641856+poolpoolloop@users.noreply.github.com>
2024-03-25 22:34:37 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+ echo'hello'
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./laravel11/resources/css/reset.css">
+    <link rel="stylesheet" href="./laravel11/resources/css/stily.css">
+    <title>GreenShop-HomePage</title>
+</head>
+<body>
+    <header></header>
+    <div class="swiper_baner"></div>
+    <body class="body_home_filter_nav_swiper">
+        <div class="filter_baner filter_baner_conteiner"> 
+          <div class="filter filter_conteiner"> 
+            <nav> 
+
+            </nav>
+          </div> 
+          <div class="baner_conteiner"> 
+            <img src="./laravel11/resources/img/galery/Super Sale Banner.jpg" alt="super sale" class="supersale">
+          </div> 
+        </div>
+        <div class="nav_swiper"> 
+          <nav class="nav"></nav>
+          <div class="swiper"></div>
+        </div>
+    </body>
+    <nav class="zero_two"> 
+      <div class="card_one"></div>
+      <div class="card_two"></div>
+    </nav>
+    <div class="blog"> 
+      <div class="text"></div>
+      <nav class="cart_blogs"> 
+        <div class="catr_blg">
+            <img src="" alt="" class="">
+            <div> 
+
+            </div>
+        </div>
+        <div class="catr_blg"></div>
+        <div class="catr_blg"></div>
+        <div class="catr_blg"></div>
+      </nav>
+    </div>
+    <footer></footer>
+    <script src="./laravel11/resources/js/app.js"></script>
+    <script src="./laravel11/resources/js/bootstrap.js"></script>
+</body>
+</html>
